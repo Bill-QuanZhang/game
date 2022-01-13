70004405800900fddd51b193b8536cc3f8447993
Bill Zhang <20030612zq@gmail.com>
2022-01-13 11:18:01 -0800

    Initial commit

---
+++ game.py
@@ -0,0 +1,13 @@
+# Author: Bill
+
+import pygame
+import time
+import random
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0,   0,   0)
+RED = (255,   0,   0)
+GREEN = (0, 255,   0)
+BLUE = (0,   0, 255)
