681b0f748e3b7bc0e2f0d60d62059061f4be0e41
Bill Zhang <20030612zq@gmail.com>
2022-01-13 11:25:06 -0800

    add classes I may use

--- game.py
+++ game.py
@@ -11,3 +11,19 @@ BLACK = (0,   0,   0)
 RED = (255,   0,   0)
 GREEN = (0, 255,   0)
 BLUE = (0,   0, 255)
+
+class Player(pygame.sprite.Sprite):
+
+class Enemy(pygame.sprite.Sprite):
+
+class Bullet(pygame.sprite.Sprite):
+
+class Wattack(pygame.sprite.Sprite):
+
+class Aattack(pygame.sprite.Sprite):
+
+class Sattack(pygame.sprite.Sprite):
+
+class Dattack(pygame.sprite.Sprite):
+
+class Platform(pygame.sprite.Sprite):
